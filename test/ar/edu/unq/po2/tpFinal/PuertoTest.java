562816ba3c2e97ae99d8e085877ac9f4a77cdec2
axlg98 <axlgarcia080498@gmail.com>
2023-10-12 20:17:12 -0300

    Agregando src y test

---
+++ test/ar/edu/unq/po2/tpFinal/PuertoTest.java
@@ -0,0 +1,22 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PuertoTest {
+	private Puerto p;
+	
+	@BeforeEach
+	void setUp() {
+		p = mock(Puerto.class);
+	}
+	
+	@Test
+	void test() {
+		//assertTrue(True);
+	}
+
+}
