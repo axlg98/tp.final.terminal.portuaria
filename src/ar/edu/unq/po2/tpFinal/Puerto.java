6572a58242a3e43ffa08a62f0dee8c0ff1b17425
axlg98 <axlgarcia080498@gmail.com>
2023-10-14 01:36:09 -0300

    Avanzando con los Get y Set de algunas Clases.

--- src/ar/edu/unq/po2/tpFinal/Puerto.java
+++ src/ar/edu/unq/po2/tpFinal/Puerto.java
@@ -4,6 +4,30 @@ import java.util.List;
 
 public class Puerto {
 	private String nombre; // nombre del puerto en un determinado pais
-	private List<Buque> buques;
+	private List<Empresa> empresas; //Lista de Empresa.
+	
+	
+	public Puerto(String nombre,List<Empresa> emps) {
+		this.setNombre(nombre);
+		this.setEmpresas(emps);
+	}
+	
+	//GET Y SET.
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Empresa> getEmpresas() {
+		return empresas;
+	}
+
+	public void setEmpresas(List<Empresa> empresas) {
+		this.empresas = empresas;
+	}
+	
 	
 }
