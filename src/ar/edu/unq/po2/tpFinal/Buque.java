af8cf8986ac4171a95ce03e02b05b47bef8a8d2a
Dario Gabriel Gomez <111874728+GomezDario@users.noreply.github.com>
2023-11-18 14:27:39 -0300

    Agregando funcionalidades en Buque

--- src/ar/edu/unq/po2/tpFinal/Buque.java
+++ src/ar/edu/unq/po2/tpFinal/Buque.java
@@ -1,19 +1,93 @@
 package ar.edu.unq.po2.tpFinal;
 
 import java.util.List;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
+
+		
 public class Buque {
-private List <Container> containers;
+ 
+	private String nombre;
+	private FaseDeBuque fase;
+	private Point2D  posicionActual = new Point2D.Double(0, 0);
+	private List<Container> containers;
 	
-	public Buque(List<Container> cs) {
-		this.setContainers(cs);
+	private Viaje unViaje;
+	
+	
+	
+	public Buque(String nombre) {
+		super();
+		this.nombre = nombre;
+		this.fase = FaseDeBuque.Outbound; // Fase inicial del buque
+		this.containers = new ArrayList<Container>();
 	}
-
+	 
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public FaseDeBuque getFase() {
+		return fase;
+	}
+	public void setFase(FaseDeBuque fase) {
+		this.fase = fase;
+	}
 	public List<Container> getContainers() {
 		return containers;
 	}
-
 	public void setContainers(List<Container> containers) {
 		this.containers = containers;
 	}
+	
+	public void agregarContainer(Container container) {
+		
+		this.getContainers().add(container);
+		
+	}
+	
+	public void cambiarDeFase(FaseDeBuque fase) {
+		
+		if (fase == FaseDeBuque.Inbound) {
+			
+			// dar aviso de cambio de fase
+			this.setFase(fase);
+		}
+		
+		
+		
+	}
+
+
+
+	public Point2D getPosicionActual() {
+		return posicionActual;
+	}
+
+
+
+	public void setPosicionActual(Point2D posicionActual) {
+		this.posicionActual = posicionActual;
+	}
+
+
+	public Viaje getUnViaje() {
+		return unViaje;
+	}
+
+
+
+	public void setUnViaje(Viaje unViaje) {
+		this.unViaje = unViaje;
+	}
+	
+	
+	
+	
+	
 }
