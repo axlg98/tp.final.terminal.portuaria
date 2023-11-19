6fb08c68a3ab76554a5635086b7e7d3da87f4651
Dario Gabriel Gomez <111874728+GomezDario@users.noreply.github.com>
2023-11-19 17:06:52 -0300

    Agregando funcionalidad estrategias mejor ruta

--- src/ar/edu/unq/po2/tpFinal/EstrategiaMenorTiempo.java
+++ src/ar/edu/unq/po2/tpFinal/EstrategiaMenorTiempo.java
@@ -1,5 +1,35 @@
 package ar.edu.unq.po2.tpFinal;
 
-public class EstrategiaMenorTiempo {
+import java.util.List;
 
+public class EstrategiaMenorTiempo extends EstrategiaMejorRuta{
+
+	@Override
+	public Circuito elMejorCircuito(Puerto puertoOrigen, Puerto puertoDestino) {
+		// TODO Auto-generated method stub
+		
+		
+		List<Circuito> circuitos = this.losCircuitos(puertoOrigen, puertoDestino);
+		Circuito elDeMenorTiempo = circuitos.get(0);
+		
+		for (Circuito circuito : circuitos) {
+			
+			if (circuito.duracionDeTodoElCircuito() < elDeMenorTiempo.duracionDeTodoElCircuito()) {
+				
+				elDeMenorTiempo = circuito;
+				
+			}
+				
+		}
+		
+		
+		return elDeMenorTiempo;
+		
+		
+		
+	}
+
+	
+	
+	
 }
