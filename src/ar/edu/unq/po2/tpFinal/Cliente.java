6d8c913995b352a75812811196f8d56c3558dd36
axlg98 <axlgarcia080498@gmail.com>
2023-11-21 16:20:23 -0300

    agregando y avanzando con algunas clases

--- src/ar/edu/unq/po2/tpFinal/Cliente.java
+++ src/ar/edu/unq/po2/tpFinal/Cliente.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Cliente {
 	
 	private List<Orden> ordenes;
+	private Container carga;
 	
 	
 	public void agregarOrden(Orden orden) {
@@ -13,5 +14,25 @@ public class Cliente {
 		
 	}
 	
+	//GET Y SET
 	 
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
+	}
+
+
+	public Container getCarga() {
+		return carga;
+	}
+
+
+	public void setCarga(Container carga) {
+		this.carga = carga;
+	}
+	
 }
---
+++ src/ar/edu/unq/po2/tpFinal/Electricidad.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.po2.tpFinal;
+
+public class Electricidad implements Servicio{
+	
+	@Override
+	public Double costoServicio(Orden orden) {
+		
+	}
+}
